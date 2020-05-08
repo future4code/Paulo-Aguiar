5098067419ebe6578947b2acc5f16d06dc1b265e
Paulo Aguiar <paulopma@hotmail.com>
2020-05-08 16:52:39 -0300

    Interpretação de código completo

---
+++ semana4/lista-de-exercicios/exercicios.js
@@ -0,0 +1,25 @@
+/*interpretação de código=======================================================
+1.----------------------------------------------------------------------------
+O código transforma dólar em real, ele tem uma função que recebe o valor em 
+dólares e multiplica pela cotação do dólar, que é inserida através do navegador.
+Saída: R$ 575
+
+2.-----------------------------------------------------------------------------
+O código faz uma previsão de quanto dinheiro o usuário terá, se fizer algum tipo
+de investimento. O tipo de investimento e o valor são recebidos por uma função
+que multiplica o valor investido dependendo do tipo de investimento.
+Se for colocado um tipo de investimento que não faz parte do código, um alerta
+é exibido para o usuário.
+Saída: 165; undefined
+
+3.-------------------------------------------------------------------------------- 
+O código itera sobre um array de números e separa os números desse array original
+em dois outro arrays, dependendo se o número é par ou ímpar.
+Saída: 14; 6; 8
+
+4.------------------------------------------------------------------------------ 
+O código determina, através das variáveis numero1 e numero2, respectivamente,
+qual o menor e o maior número do array números. Ele faz isso iterando sobre o
+array e dando duas condicionais.
+Saída: -10; 1590 */
+
