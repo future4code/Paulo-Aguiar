e254a548adf4883c950d4d22f8e83e11c58bbdd5
Paulo Aguiar <paulopma@hotmail.com>
2020-05-13 16:40:03 -0300

    react state obrigatorio

---
+++ semana5/aula18/labedin/build/precache-manifest.395dd267ecb94153e27d0cbd92a5d2a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae983d8a394e817eced0dcd7dd2868a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "318403b97069ad0104ba",
+    "url": "/static/css/main.acce2ee6.chunk.css"
+  },
+  {
+    "revision": "dd35cab1ec671b13bbed",
+    "url": "/static/js/2.65cf7749.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65cf7749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318403b97069ad0104ba",
+    "url": "/static/js/main.0cace1c2.chunk.js"
+  },
+  {
+    "revision": "9cec4286d4e96efb4903",
+    "url": "/static/js/runtime-main.6b07746c.js"
+  }
+]);
